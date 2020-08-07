ce9b0f14c7412d236cb1f1c3144d271c27a194af
MarcoCPinho <marcocpinho@hotmail.com>
2020-08-07 18:05:14 -0300

    updates

---
+++ src/database/fake_data.js
@@ -0,0 +1,37 @@
+//data
+
+const proffys = [
+{
+    name: "Marco Pinho",
+    avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQHUoaWQSgOH9Q/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=SwoqMUT2Hh0S8gfFITc8qIdmAE42guMBvby3A1X8cI0",
+    whatsapp: "47991118981",
+    bio: "Aprendendo desenvolvimento front-end",
+    subject: "Ciência",
+    cost: "150",
+    weekday: [1],
+    time_from: [1980],
+    time_to: [5220]
+},
+{
+    name: "Marco C Pinho",
+    avatar: "https://avatars0.githubusercontent.com/u/68476808?s=460&u=e46db5c112259f8f91aebb0a9732b6e492e9af59&v=4",
+    whatsapp: "47991118981",
+    bio: "Aprendendo desenvolvimento back-end",
+    subject: "Física",
+    cost: "320",
+    weekday: [3],
+    time_from: [1980],
+    time_to: [5220]
+},
+{
+    name: "Marco Carvalho de Pinho",
+    avatar: "https://avatars0.githubusercontent.com/u/68476808?s=460&u=e46db5c112259f8f91aebb0a9732b6e492e9af59&v=4",
+    whatsapp: "47991118981",
+    bio: "Aprendendo desenvolvimento full-stack",
+    subject: "Matemática",
+    cost: "410",
+    weekday: [5],
+    time_from: [1980],
+    time_to: [5220]
+}
+]
